69b01b74ac0c51c71d25ca4a1d5ebd397151cf65
Carmenamalia <138214555+Carmenamalia@users.noreply.github.com>
2023-12-21 18:29:22 +0200

    Add files via upload

---
+++ cinemacityclone/src/main/java/com/springapps/cinemacityclone/CinemacitycloneApplication.java
@@ -0,0 +1,13 @@
+package com.springapps.cinemacityclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemacitycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemacitycloneApplication.class, args);
+	}
+
+}
---
+++ cinemacityclone/src/test/java/com/springapps/cinemacityclone/CinemacitycloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapps.cinemacityclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemacitycloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
